8ca49b87959ed843c269494a7cf64237473659ec
Matthew Wiessing <matthewwiessing@gmail.com>
2021-12-11 02:44:51 -0600

    made service worker file following example in activity made db.js file

---
+++ public/service-worker.js
@@ -0,0 +1,89 @@
+console.log("Service worker is being used right now");
+
+const STATIC_CACHE = "static-cache-v2";
+const RUNTIME_CACHE = "runtime-cache";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/db.js",
+  "/icons/icon-128x128.png",
+  "/icons/icon-512x512.png",
+];
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(STATIC_CACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(() => {
+        console.log(` NOTICE THIS! Service worker has been installed!`);
+        self.skipWaiting();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter(
+          (cacheName) => !currentCaches.includes(cacheName)
+        );
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+      .catch((err) => {
+        console.log(err);
+      })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (
+    event.request.method !== "GET" ||
+    !event.request.url.startsWith(self.location.origin)
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+  if (event.request.url.includes("/")) {
+    event.respondWith(
+      caches.open(RUNTIME_CACHE).then((cache) => {
+        return fetch(event.request)
+          .then((response) => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => caches.match(event.request));
+      })
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return caches.open(RUNTIME_CACHE).then((cache) => {
+        return fetch(event.request).then((response) => {
+          return cache.put(event.request, response.clone()).then(() => {
+            return response;
+          });
+        });
+      });
+    })
+  );
+});
